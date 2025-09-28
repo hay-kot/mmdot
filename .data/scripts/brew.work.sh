ced05644fe6b0a462f0f86cfd31ed1d218127f19
Hayden <64056131+hay-kot@users.noreply.github.com>
2025-09-27 23:24:07 -0500

    update testfile for dev and do rendering

---
+++ .data/scripts/brew.work.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+# Exit if any command in a pipeline fails (not just the last one)
+set -o pipefail
+# Treat unset variables as an error when substituting
+set -u
+# Adding Homebrew Taps
+brew tap all:taps
+brew tap work:taps
+
+# Installing Homebrew Packages
+brew install \
+  all:brews \
+  work:brews
+
+# Installing Homebrew Casks
+brew install --cask \
+  all:casks \
+  work:casks
+
