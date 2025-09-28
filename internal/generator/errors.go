9f83e4887fd9dce02985ac3f6d349d206c911007
Hayden <64056131+hay-kot@users.noreply.github.com>
2025-09-27 23:29:12 -0500

    fix

--- internal/generator/errors.go
+++ internal/generator/errors.go
@@ -70,7 +70,9 @@ func (te *TemplateError) loadContext() {
 	if err != nil {
 		return
 	}
-	defer file.Close()
+	defer func() {
+		_ = file.Close()
+	}()
 
 	scanner := bufio.NewScanner(file)
 	lineNum := 1
