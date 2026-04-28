bd950d87aa1d35be86950f62b16b8d6a56a2ce9c
Hayden <64056131+hay-kot@users.noreply.github.com>
2026-04-28 17:09:23 +0100

    feat: add LoadPublicKeys bulk loader for multiple recipients
    closes hc-vs8pu1u4

--- pkgs/fcrypt/key_loaders.go
+++ pkgs/fcrypt/key_loaders.go
@@ -15,6 +15,20 @@ func LoadPublicKey(key string) (*age.X25519Recipient, error) {
 	return ageRecipient, nil
 }
 
+func LoadPublicKeys(keys []string) ([]age.Recipient, error) {
+	recipients := make([]age.Recipient, 0, len(keys))
+	for _, key := range keys {
+		recipient, err := LoadPublicKey(key)
+		if err != nil {
+			return nil, err
+		}
+
+		recipients = append(recipients, recipient)
+	}
+
+	return recipients, nil
+}
+
 func LoadPrivateKey(key string) (*age.X25519Identity, error) {
 	ageIdentity, err := age.ParseX25519Identity(key)
 	if err != nil {
